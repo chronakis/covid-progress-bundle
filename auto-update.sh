98339d5870e2a2ae6224614a24dbe27d5eec29d0
Ioannis Chronakis <ioannis.chronakis@deontics.com>
2020-04-14 14:41:13 +0100

    Added aws command but works only on windows now because of paths and profiles

--- auto-update.sh
+++ auto-update.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
-git_keyword="Already"
-#git_keyword="csse_covid_19_daily_reports"
+#export AWS_PROFILE=covid
+export JAVA_HOME=/c/Java/jdk-9
+export PATH=/c/Java/jdk-9/bin:$PATH
 
-git -C COVID-19 pull | grep $git_keyword && updates=true
+AWS="aws --profile covid"
+BUCKET=covid-progress.net
 
-if [ "$updates" != "true" ]; then
+#git_keyword="Already"
+git_keyword="csse_covid_19_daily_reports"
+
+git -C COVID-19 pull | grep $git_keyword && UPDATES_PRESENT=true
+
+# Debug
+UPDATES_PRESENT=true
+
+if [ "$UPDATES_PRESENT" != "true" ]; then
 	exit 1
 fi
 
 cd covid-data-processor
 ./run.sh
 cd ../covid-ui
-git st
+$AWS s3 sync data s3://covid-progress.net/data
